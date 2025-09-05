0861c7a26c260581171ff3dfd022096f9dbe7360
charrafimed <mhch5003@gmail.com>
2025-09-05 17:05:05 +0200

    after init command

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
+import './globals/theme.js'; /* By Sheaf.dev */ 
+
 import './bootstrap';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+
+// now you can register
+// components using Alpine.data(...) and
+// plugins using Alpine.plugin(...) 
+
+
+ 
+Livewire.start()
